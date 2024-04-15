f20ed9f007bc0812e53a73b6b3296feeda0beabd
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2024-04-15 15:18:11 +0700

    feat: use dynamic env

--- src/lib/okane-finance-api/oakne-client.ts
+++ src/lib/okane-finance-api/oakne-client.ts
@@ -1,13 +1,11 @@
 import {
-	OKANE_FINANCE_API_URL,
-	OKANE_FINANCE_API_USER,
-	OKANE_FINANCE_API_PASSWORD
-} from '$env/static/private';
+	env
+} from '$env/dynamic/private';
 import { Configuration, SignalsApi } from './generated';
 
 const config = new Configuration({
-  basePath: OKANE_FINANCE_API_URL,
-  username: OKANE_FINANCE_API_USER,
-  password: OKANE_FINANCE_API_PASSWORD
+  basePath: env.OKANE_FINANCE_API_URL,
+  username: env.OKANE_FINANCE_API_USER,
+  password: env.OKANE_FINANCE_API_PASSWORD
 })
 export const okaneClient = new SignalsApi(config);
