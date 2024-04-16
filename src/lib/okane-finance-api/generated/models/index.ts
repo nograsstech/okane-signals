08c05ad6763225068e7d2fe7ccce913b04565f79
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2024-04-16 22:15:19 +0700

    feat: update okane finance api generated code

--- src/lib/okane-finance-api/generated/models/index.ts
+++ src/lib/okane-finance-api/generated/models/index.ts
@@ -1,11 +1,14 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from './AlphaVantageNewsResponseDTO';
+export * from './BacktestResponseDTO';
+export * from './BacktestStats';
 export * from './End';
 export * from './HTTPValidationError';
 export * from './Limit';
 export * from './Parameters';
 export * from './Period';
+export * from './ResponseBacktestSignalsBacktestGet';
 export * from './RootResponse';
 export * from './Signal';
 export * from './SignalRequestData';
