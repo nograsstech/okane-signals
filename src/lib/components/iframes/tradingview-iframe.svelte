b5603785554b5c8e883a03e6d149a50d6e989758
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2024-07-26 19:30:17 +0700

    fix: tradingview widget

--- src/lib/components/iframes/tradingview-iframe.svelte
+++ src/lib/components/iframes/tradingview-iframe.svelte
@@ -1,49 +1,32 @@
-<script lang="ts">
-	import { afterUpdate, onDestroy, onMount } from 'svelte';
-
-	export let app;
-
-	onMount(() => {
-		console.log(`Mounted ${app}`);
-	});
-
-	afterUpdate(() => {
-		console.log(`Updated ${app}`);
-	});
-
-	onDestroy(() => {
-		console.log(`Destroyed ${app}`);
-	});
+<script>
+  function createWidget() {
+    return (`<div class="tradingview-widget-container">
+        <div class="tradingview-widget-container__widget"></div>
+        <script
+          type="text/javascript"
+          src="https://s3.tradingview.com/external-embedding/embed-widget-events.js"
+          async
+        >
+        {
+          "colorTheme": "dark",
+          "isTransparent": false,
+          "width": "100%",
+          "height": "550",
+          "locale": "en",
+          "importanceFilter": "-1,0,1",
+          "countryFilter": "ar,au,br,ca,cn,fr,de,in,id,it,jp,kr,mx,ru,sa,za,tr,gb,us,eu"
+        }
+        <\/script>
+      </div>`);
+  }
 </script>
-
-<!-- <iframe title={app} id="microapp-{app}" frameborder="0" scrolling="no" src={url}></iframe> -->
-
-<!-- TradingView Widget BEGIN -->
-<div class="tradingview-widget-container">
-	<div class="tradingview-widget-container__widget"></div>
-	<script
-		type="text/javascript"
-		src="https://s3.tradingview.com/external-embedding/embed-widget-events.js"
-		async
-	>
-  {
-  "colorTheme": "dark",
-  "isTransparent": false,
-  "width": "100%",
-  "height": "550",
-  "locale": "en",
-  "importanceFilter": "-1,0,1",
-  "countryFilter": "ar,au,br,ca,cn,fr,de,in,id,it,jp,kr,mx,ru,sa,za,tr,gb,us,eu"
-}
-	</script>
-</div>
-
-<!-- TradingView Widget END -->
 
 <!-- svelte-ignore css-unused-selector -->
 <style>
-	iframe {
-		width: 100%;
-		height: 1000px;
-	}
+  iframe {
+    width: 100%;
+    height: 1000px;
+  }
 </style>
+
+{@html createWidget()}
